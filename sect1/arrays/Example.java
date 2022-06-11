b0685671c64140df8819bc2c06634b0971e69587
Mark Harmon <neothej1sec@gmail.com>
2022-06-11 16:11:26 -0500

    finished first 2 lessons on arrays

---
+++ sect1/arrays/Example.java
@@ -0,0 +1,46 @@
+// public class Example{
+//     public static void main(String[] args){
+//         int[] marks = {78,93,72,81,86};
+//         System.out.println(marks[1]);
+//     }
+// }
+
+//manipulating an array
+// public class Example{
+//     public static void main(String[] args){
+//         String[] javaStudents = {"Mike", "Sam", "LEo", "Arjun", "PErry", "Nikki"};
+//         javaStudents[3] = "Potato Dan";
+//         System.out.println(javaStudents[3]);
+//     }
+// }
+
+//getting the length of an array
+// public class Example{
+//     public static void main(String[] args){
+//         int[] stonch = {4,5,6,7,8};
+//         System.out.println("The lenght is " + beers.length);
+//     }
+// }
+
+// looping an array length
+// public class Example{
+//     public static void main(String[] args){
+//         String[] snookiesCookies = {"asdf", "wert", "qwerty", "bnm,."};
+//         for(int i=0; i<snookiesCookies.length; i++){
+//             System.out.println(snookiesCookies[i]);
+//         }
+//     }
+// }
+
+// looping until found in array
+// public class Example{
+//     public static void main(String[] args){
+//         int[] numbers = {1,2,4,6,8,19,25,63,85,90};
+//         for(int i=0; i<numbers.length; i++){
+//             if (numbers[i] == 25){
+//                 break;
+//             }
+//             System.out.println(numbers[i]);
+//         }
+//     }
+// }
--- sect1/arrays/readme.md
+++ sect1/arrays/readme.md
@@ -1,2 +1,22 @@
 # Java Arrays:
-* This file holds sect 7 Arrays
+***   
+There are situations where we need to store the similar or correlated data sucha as marks of a student in five subjects. This can be achieved by jest declaring five different variables. But this has drawbacks:
+* more memory will be required since the new block of memory space will be allocated 
+    to every variable
+* Processing and accessing this data would become touch since we would need to refer 
+    them using their different variable names
+* each element (variable) here would be independant. Therefore, it would be tough to 
+    correlate those variables with a particular student
+#### Arrays are the answer to these drawbacks
+#### Array:
+* is a data structure that allows us to store multiple values in a single variable.
+    in simple words we can group various data together into groups
+* an array is a fixed-size sequential colection of elements. These element have to be 
+    of a similar type. Meaning an array can only store one data type but many of values in that form
+##### SYNTAX !!!!!
+data_type[] array_name;
+array_name = new data_type[size];
+|| OR ||
+* you can combine those steps into one
+data_type[] array_name = new data_type[size];
+ex: int[] marks = new int[5];
