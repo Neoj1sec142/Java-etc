e026de9948043fcb2b860669805ff62d700df207
Mark Harmon <neothej1sec@gmail.com>
2022-06-11 01:49:38 -0500

    finished chapter on iteration 100% on test

---
+++ sect1/iteration/loops/Example.java
@@ -0,0 +1,66 @@
+// simple FOR loop
+// public class Example{
+//     public static void main(String[] args){
+//         for(int i=0; i<5; i++){
+//             System.out.println("Java is long and grating");
+//         }
+//     }
+// }
+
+// simple WHILE loop
+// public class Example{
+//     public static void main(String[] args){
+//         int i = 0;
+//         while(i<5){
+//             System.out.println("Java");
+//             i++;
+//         }
+//     }
+// }
+
+// simple DO WHILE loop
+// public class Example{
+//     public static void main(String[] args){
+//         int i = 0;
+//         do{
+//             System.out.println("Ehhhhh");
+//             i++;
+//         }while(i<5);
+//     }
+// }
+
+// breaking a loop with the break keyword
+// public class Example{
+//     public static void main(String[] args){
+//         for(int i=1; i<=10; i++){
+//             if(i == 6){
+//                 break;
+//             }
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+// creating the multiplication table
+// public class Example{
+//     public static void main(String[] args){
+//         int num = 5;
+//         for(int i=1; i<=10; i++){
+//             System.out.println(num + " X " + i + " = " + num*i);
+//         }
+//     }
+// }
+
+// print nums in specific range
+// public class Example{
+//     public static void main(String[] args){
+//         int num = 10;
+//         int i = 0;
+//         while(i<=num){
+//             if(i%2==0){
+//                 System.out.println(i);
+//             }
+//             i++;
+//         }
+//     }
+// }
--- sect1/iteration/loops/loops.md
+++ sect1/iteration/loops/loops.md
@@ -9,3 +9,30 @@
 * For loop 
 * While Loop
 * Do While loop 
+
+### For Loops:
+##### SYNTAX ##### 
+for(initilization; condition; increment/decrement){
+    //body of loop
+}
+
+* Initialization - marks the start of a for loop and this part is executed only once
+        he we can decalre and init a looping variable
+* Condition - evaluates to a boolean value. if the boolean is returned true the Loop
+        is executed otherwise the body of the loop will not be executed and the control jumps to the next statment after the for loop
+* Update Expression(incre/decre) - is used for updating the variable for the next
+        iteration. if the conditio is true the body of the for loops gets executed
+        the control jumps back up to the update statment which updates the value of the lop variable
+
+### While Loops:
+##### SYNTAX #####
+while(condition){
+    //body of the Loop
+    //update the condition of the loop
+}
+
+### DO WHILE Loops:
+##### SYNTAX #####
+do{
+    //body of loop
+}while(condition);
