9d7c0dd2fe488fbf0d7b5b4b1ecfe065c18149f2
Mark Harmon <neothej1sec@gmail.com>
2022-06-12 21:46:51 -0500

    started project 1 in java

---
+++ sect1/proj1/Test.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+// public class Test{
+//     public static void main(String[] args){
+//         String username;
+//         Scanner input = new Scanner(System.in);
+//         System.out.println("Enter your name:");
+//         username = input.nextLine();
+//         System.out.println("Hello " + username);
+//         input.close();
+//     }
+// }
+
+// public class Test{
+//     public static void main(String[] args){
+//         int numOne, numTwo;
+//         Scanner input = new Scanner(System.in);
+//         System.out.println("Enter the 1st number: ");
+//         numOne = input.nextInt();
+//         System.out.println("Enter the 2nd number: ");
+//         numTwo = input.nextInt();
+//         System.out.println("Addition is, " + (numOne + numTwo));
+//         input.close();
+//     }
+// }
--- sect1/proj1/readme.md
+++ sect1/proj1/readme.md
@@ -1,1 +1,9 @@
 # Project 1 Java:
+a small investment tracker app
+Scanner = class for user input
+## methods of scanner class:
+nextLine() - reads the string value from the user (basic user input)
+nextInt() - reads the int(integer) value from the user
+nextDouble() - reads the double value from the user
+nextFloat() - reads the float (floating-point) value from the user
+nextBoolean() - reads the boolean value from the user
